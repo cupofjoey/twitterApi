8a100a40e198ad5c8e667bd7ed20ab62645bb373
Ken Anthony <ken@Kens-MacBook-Pro.local>
2016-01-06 22:18:01 -0800

    TweetCount stream.js able to stream tweets with 'sanders' mention, and count mentions

--- stream.js
+++ stream.js
@@ -1,5 +1,6 @@
 var Twit = require('twit');
 var config = require('./config');
+var fs = require('fs');
 
 var T = new Twit(config);
 
@@ -7,8 +8,24 @@ var stream = T.stream('statuses/filter', { track: 'sanders'})
 
 
 var streaming = function() {
+	var count = 0;
+
+	function increment() {
+		count ++;
+	};
+
 	stream.on('tweet', function(tweet) {
 		console.log(tweet.text)
+		if(tweet) {
+			increment();
+
+		}
+
+		fs.writeFile('tweetCount.txt', count, function(err) {
+			if(err) {
+				throw err;
+			}
+		})
 	})
 }
 
